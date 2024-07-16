b7946137096d18161bce5cd81379b9815e7b62c6
Deepesh Mitra <friends.deepesh@gmail.com>
2024-07-16 21:28:37 +0530

    First commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,81 @@
+"use client";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import NextLink from "next/link";
+import { usePathname } from "next/navigation";
+import { FC, useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+const Links = [
+  {
+    href: "/",
+    name: "About me",
+    id: "about-me",
+  },
+  {
+    href: "#projects",
+    name: "Projects",
+    id: "projects",
+  },
+  {
+    href: "#experience",
+    name: "Experience",
+    id: "experience",
+  },
+  {
+    href: "#skills",
+    name: "Skills",
+    id: "skills",
+  },
+];
+const Navbar: FC = () => {
+  const pathname = usePathname();
+
+  const [activeSection, setActiveSection] = useState(Links[0].href);
+  // const isActive = (path: string) => pathname === path;
+
+  const handleScroll = () => {
+    let lastActiveElement = null;
+    Links.forEach((link) => {
+      const element = document.getElementById(link.id);
+      let elememtOffset = element?.offsetTop;
+      const scrollPosition = window.scrollY;
+      if (elememtOffset != null && scrollPosition > elememtOffset) {
+        lastActiveElement = link.href;
+      }
+    });
+    if (lastActiveElement) setActiveSection(lastActiveElement);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  return (
+    <nav className="py-4 px-8 h-20 ">
+      <div className="gap-8 flex flex-row justify-between items-center h-full">
+        <div>
+          <div>Logo</div>
+          <div>SMARTLY</div>
+        </div>
+        <div className="flex flex-row gap-8 font-semibold">
+          <div>Solutions</div>
+          <div>Platform</div>
+          <div>Testimonials</div>
+        </div>
+        <div className="h-full">
+          <Button className="bg-Electric-Violet rounded-full h-full px-7 py-4 font-grotesk_wide text-base font-semibold">
+            Get Demo
+          </Button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ lib/fonts.ts
@@ -0,0 +1,84 @@
+import localFont from "next/font/local";
+export const grotesk = localFont({
+  src: [
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgrotesk-extralight.otf",
+      weight: "100",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgrotesk-extralight.otf",
+      weight: "200",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgrotesk-light.otf",
+      weight: "300",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgrotesk-regular.otf",
+      weight: "400",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgrotesk-medium.otf",
+      weight: "500",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgrotesk-bold.otf",
+      weight: "600",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgrotesk-extrabold.otf",
+      weight: "700",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgrotesk-black.otf",
+      weight: "800",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgrotesk-heavy.otf",
+      weight: "900",
+    },
+  ],
+  variable: "--font-grotesk",
+});
+
+export const groteskWide = localFont({
+  src: [
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgroteskwide-extralight.otf",
+      weight: "100",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgroteskwide-extralight.otf",
+      weight: "200",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgroteskwide-light.otf",
+      weight: "300",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgroteskwide-regular.otf",
+      weight: "400",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgroteskwide-medium.otf",
+      weight: "500",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgroteskwide-bold.otf",
+      weight: "600",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgroteskwide-extrabold.otf",
+      weight: "700",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgroteskwide-black.otf",
+      weight: "800",
+    },
+    {
+      path: "../assets/fonts/grotesk/Fontspring-DEMO-rocgroteskwide-heavy.otf",
+      weight: "900",
+    },
+  ],
+  variable: "--font-grotesk_wide",
+});
