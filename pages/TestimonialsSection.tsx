7bde3714573025253090602c9cf7e93dc9b6cb8f
Deepesh Mitra <friends.deepesh@gmail.com>
2024-08-03 16:27:30 +0530

    Testimonials

--- pages/TestimonialsSection.tsx
+++ pages/TestimonialsSection.tsx
@@ -12,8 +12,20 @@ const testimonial = [
   {
     text: "It’s been nice working with AdYugo. They opened more demand sources to increase our inventory yield without SDK integration. We are hopeful the business will keep the momentum in months and years to follow.",
     name: "Kaushik Patel",
-    img: "/testimonial-1.jpeg",
+    img: "/testimonials/testimonial-1.jpeg",
     designation: "Co-founder, Tathastu Technologies",
+  },
+  {
+    text: "From the moment we engaged with AdYugo, their hands-on approach, speed of response, and proactive insight into adops has been world class. They’ve played a big part in helping us better monetise our audience and have been a hugely valuable addition to our stack.",
+    name: "Kalpesh Patel",
+    img: "/testimonials/testimonial-2.jpeg",
+    designation: "CEO, KPEworld Media",
+  },
+  {
+    text: "AdYugo put more effort into understanding my website and the results speak for themselves. They are responsive and thorough. Not all partners are the same.",
+    name: "Mustapha",
+    img: "/testimonials/testimonial-3.jpeg",
+    designation: "Ad Monetization manager, Lemozik",
   },
 ];
 
@@ -26,7 +38,7 @@ const TestimonialsSection = (props: Props) => {
       <h2 className="my-24 max-w-[1000px] text-4xl lg:text-5xl text-center leading-[3rem] tracking-tight font-semibold  ">
         <span className="text-Mardi-Gras">What our client says</span>
       </h2>
-      <div className="flex flex-wrap items-center justify-center">
+      <div className="flex flex-wrap flex-col xl:flex-row gap-7  justify-center">
         {testimonial.map((val, index) => (
           <TestimonialCard key={index} {...val} />
         ))}
