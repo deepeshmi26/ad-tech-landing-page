5bca7cfb57d7c2b9e433d0b14966dc1d26dc629b
Deepesh Mitra <friends.deepesh@gmail.com>
2024-08-05 12:55:17 +0530

    Footer update

--- pages/FooterSection.tsx
+++ pages/FooterSection.tsx
@@ -16,53 +16,82 @@ const FooterSection = (props: Props) => {
         id={"home"}
         className="bg-gradient-to-r from-blue-800 to-indigo-900 px-8 py-8  text-white mt-8 gap-8 flex-col flex items-center justify-center w-full"
       >
-        <NextLink href="#home">
-          <Image src="/logo.png" width={275 / 2} height={87 / 2} alt="Adyugo" />
-        </NextLink>
-        <div className=" max-w-[1220px] w-full flex flex-col items-center justify-center md:flex-row gap-8">
-          <section className="flex flex-col gap-8">
-            <div className="flex flex-row gap-6 ">
-              <Button
-                variant={"ghost"}
-                size={"icon"}
-                className="text-gray-700 hover:text-primary"
-              >
-                <NextLink
-                  href={"https://www.linkedin.com/company/adyugo-media/"}
+        <div className="w-full flex flex-col md:flex-row gap-6">
+          <NextLink className="basis-1/3" href="#home">
+            <Image src="/logo.png" width={275} height={87} alt="Adyugo" />
+          </NextLink>
+          <div className=" basis-2/3 max-w-[1220px] w-full flex flex-col  justify-end gap-6">
+            <h1 className="text-xl">Get in touch with us</h1>
+            <div className="flex flex-col   gap-6">
+              <span>
+                Email:{" "}
+                <a
+                  href="mailto:webmaster@example.com"
+                  className="underline decoration-accent"
                 >
-                  <img height={35} width={35} src="social-icons/linkedin.png" />
-                </NextLink>
-              </Button>
-              <Button
-                variant={"ghost"}
-                size={"icon"}
-                className="text-gray-700 hover:text-primary"
-              >
-                <NextLink href={"https://www.instagram.com/adyugomedia/"}>
-                  <img
-                    height={35}
-                    width={35}
-                    src="social-icons/instagram.webp"
-                  />
-                </NextLink>
-              </Button>
-              <Button
-                variant={"ghost"}
-                size={"icon"}
-                className="text-gray-700 hover:text-primary"
-              >
-                <NextLink
-                  href={
-                    "https://www.facebook.com/share/kKmVHDvRp9T45D2D/?mibextid=LQQJ4d"
-                  }
-                >
-                  <img height={35} width={35} src="social-icons/facebook.png" />
-                </NextLink>
-              </Button>
+                  business@adyugo.com
+                </a>
+              </span>
+              <span>
+                Mobile:{" "}
+                <span className="underline decoration-accent">
+                  +91 9380275215
+                </span>
+              </span>
             </div>
-          </section>
+
+            <section className="flex flex-col gap-8">
+              <div className="flex flex-row gap-6 ">
+                <Button
+                  variant={"ghost"}
+                  size={"icon"}
+                  className="text-gray-700 hover:text-primary"
+                >
+                  <NextLink
+                    href={"https://www.linkedin.com/company/adyugo-media/"}
+                  >
+                    <img
+                      height={35}
+                      width={35}
+                      src="social-icons/linkedin.png"
+                    />
+                  </NextLink>
+                </Button>
+                <Button
+                  variant={"ghost"}
+                  size={"icon"}
+                  className="text-gray-700 hover:text-primary"
+                >
+                  <NextLink href={"https://www.instagram.com/adyugomedia/"}>
+                    <img
+                      height={35}
+                      width={35}
+                      src="social-icons/instagram.webp"
+                    />
+                  </NextLink>
+                </Button>
+                <Button
+                  variant={"ghost"}
+                  size={"icon"}
+                  className="text-gray-700 hover:text-primary"
+                >
+                  <NextLink
+                    href={
+                      "https://www.facebook.com/share/kKmVHDvRp9T45D2D/?mibextid=LQQJ4d"
+                    }
+                  >
+                    <img
+                      height={35}
+                      width={35}
+                      src="social-icons/facebook.png"
+                    />
+                  </NextLink>
+                </Button>
+              </div>
+            </section>
+          </div>
         </div>
-        <div>
+        <div className="text-xs">
           Copyright © 2024 - 2034 Adyugo Media LLP. All rights reserved.
         </div>
       </div>
