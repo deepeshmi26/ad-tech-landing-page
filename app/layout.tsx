1ab9f53316576bacc1529fe05fb6adadcf17aab2
Deepesh Mitra <friends.deepesh@gmail.com>
2024-08-22 21:39:16 +0530

    Gtm added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { grotesk, groteskWide } from "@/lib/fonts";
 import { Toaster } from "@/components/ui/toaster";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+
 export const metadata: Metadata = {
   title: "Adyugo",
   description: "Transform your digital inventory into profitable business",
@@ -15,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta charSet="UTF-8"></meta>
+      <GoogleTagManager gtmId="G-CH7FRP4RVW" />
       <body className={`${grotesk.variable} w-dvw ${groteskWide.variable}`}>
         {children}
         <Toaster />
